baf64b443c46a3ad6d33a1ab4761b0d54537bba5
Jamie Lewis Stax <107413598+jamielewisstax@users.noreply.github.com>
2022-09-14 22:06:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello!")
+city_name = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your first pet?\n")
+print("Your band name could be: " + city_name + " " + pet_name)
